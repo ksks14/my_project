8afd6b1fdc737cc7cd72881bab21ffb7254ae6d1
ksks14 <2061247418@qq.com>
2022-06-10 14:04:26 +0800

    测试demo

--- 深度学习demo/aio_garbge/demo.py
+++ 深度学习demo/aio_garbge/demo.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 from io import BytesIO
 from PIL import Image
 from utils.model import padding_black, val_tf
@@ -8,15 +9,13 @@ file_path = './data/img/test_tra_1.png'
 img = cv.imread(file_path)
 
 success, encoded_image = cv.imencode(".png", img)
-img_byte = img.tobytes()
 img_bytes = encoded_image.tobytes()
-
 
 img = Image.open(BytesIO(img_bytes))
 img = img.convert('RGB')
-img = padding_black(img)
-img = val_tf(img)
-img = unsqueeze(img, 0)
+img = np.array(img)
+# img = padding_black(img)
+# img = val_tf(img)
 
 
 print(img)
