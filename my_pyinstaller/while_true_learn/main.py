f40ffc749a4f429a19a94388e426ea041b6bd2ea
ksks14 <2061247418@qq.com>
2022-06-14 19:21:41 +0800

    更新

---
+++ my_pyinstaller/while_true_learn/main.py
@@ -0,0 +1,18 @@
+from time import sleep
+from cv2 import imread, imshow, waitKey, destroyAllWindows
+
+
+def my_win_sleep(time=2):
+    """
+
+    :param time:
+    :return:
+    """
+    while True:
+        print('learn please')
+        sleep(time)
+
+
+
+if __name__ == '__main__':
+    pass
